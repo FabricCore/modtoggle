6187a79ba23d85c28edc817e84c5242c782bdd15
siriusmart <siriusmart@tuta.io>
2025-04-10 23:21:49 +0100

    added new commands

--- commands/index.js
+++ commands/index.js
@@ -2,6 +2,13 @@ Command.register({
     package: "modules/toggle/commands",
     name: "toggle",
 
+    args: {
+        name: {
+            type: StringArgumentType.word(),
+            execute: "toggle.js",
+        },
+    },
+
     subcommands: {
         enable: {
             name: "enable",
@@ -18,6 +25,25 @@ Command.register({
                 name: {
                     type: StringArgumentType.word(),
                     execute: "disable.js",
+                },
+            },
+        },
+        toggle: {
+            name: "toggle",
+            args: {
+                name: {
+                    type: StringArgumentType.word(),
+                    execute: "toggle.js",
+                },
+            },
+        },
+        list: {
+            name: "list",
+            execute: "listOne.js",
+            args: {
+                page: {
+                    type: IntegerArgumentType.integer(1),
+                    execute: "list.js",
                 },
             },
         },
